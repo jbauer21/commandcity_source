1c370f506419f1e2f6b3e20f9f2998032f7e7e4f
Julian Bauer <jbauer1@trinity.edu>
2022-06-19 21:20:21 -0700

    added functionality to change directories

--- src/main/scala/replTest.scala
+++ src/main/scala/replTest.scala
@@ -31,6 +31,12 @@ object replTest {
         root.locationName = "/"
         home.parentDirectory = root
         root.pathways = List(home)
+        val testFile:Location = new Location
+        testFile.locationName = "test"
+        val nothing:Location = new Location
+        testFile.pathways = List(nothing)
+        testFile.parentDirectory = home
+        home.pathways = List(testFile)
         //-----------------------
 
     class player  {
@@ -40,9 +46,8 @@ object replTest {
         var current_working_directory:Location = root
         var home_directory:Location = null
 
-        
-        def cd(s:String):Unit = {
-
+        def cd(s:String, root:Location):Unit = {
+            val store = s.split("/").filterNot(_ == "")
             s match{
                 case "/" => {current_working_directory = root}
                 case "~" => {current_working_directory = home_directory}
@@ -55,7 +60,58 @@ object replTest {
                     }
                 } 
                 case _ => {
+                    //absolute pathing
+                    if(s.charAt(0) == '/'){
+                        //only difference between absolute and relative pathing: pwd begins from the root.
+                        current_working_directory = root
+                        breakable{
+                            for(i<-store){
+                                if(i == ".." && current_working_directory.parentDirectory != null){
+                                    current_working_directory = current_working_directory.parentDirectory
+                                }
+                                else if(i != ".."){
+                                for(j <- current_working_directory.pathways){
+                                    if(i == j.locationName){
+                                        current_working_directory = j
+                                    }
+                                else if(i == "."){
+                                    current_working_directory = current_working_directory
+                                }
+                                    else{
+                                        println("Directory not found: " + s)
+                                        break()
+                                    }
+                                }
+                                }
+                            }
+                            }
+                        }
+
                     
+                    //relative pathing
+                    else{
+                        breakable{
+                            for(i<-store){
+                                if(i == ".." && current_working_directory.parentDirectory != null){
+                                    current_working_directory = current_working_directory.parentDirectory
+                                }
+                                else if(i != ".."){
+                                for(j <- current_working_directory.pathways){
+                                    if(i == j.locationName){
+                                        current_working_directory = j
+                                    }
+                                else if(i == "."){
+                                    current_working_directory = current_working_directory
+                                }
+                                    else{
+                                        println("Directory not found: " + s)
+                                        break()
+                                    }
+                                }
+                                }
+                                }
+                            }
+                    }
                 }
                 
 
@@ -84,21 +140,22 @@ object replTest {
         //-----------------------
         var using = true
 
-        val options:Options = new Options();
-        val parser = new DefaultParser()
-        
-        
-        options.addOption("exit", "e", false, "Allows user to exit program")
-        //println(options)
-        val data = readLine()
-        val store = data.split(" ")
-        
-        val cmd:CommandLine = parser.parse(options,store)
-        if(cmd.hasOption("exit") || cmd.hasOption("e")){
-            println("works!")
-        }
+         while(using){
+            val data = readLine
+            //store should never be more than 2 long. I.E. "cd /test", only two arguments.
+            val store = data.split(" ")
+            val command = store(0)
+            
+            command match{
+                case "cd" => {var info:String = store(1); player.cd(info, root)}
+                case "pwd" => {println(player.current_working_directory.locationName)}
+                case "ls" => {player.ls}
+                case "exit" => {using = false}
+                case _ => {println(command + " is not a valid command!")}
+            }
 
         }
     }
 
 
+}
